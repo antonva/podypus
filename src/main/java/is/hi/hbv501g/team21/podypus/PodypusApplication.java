0c63d203a763a97e20a471242400a27b49a85438
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2019-10-10 10:29:39 +0000

    Initial import from spring initializr

---
+++ src/main/java/is/hi/hbv501g/team21/podypus/PodypusApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.team21.podypus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodypusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodypusApplication.class, args);
+	}
+
+}
