f324d05efad802fe8fdf90c9c96a623b97bb8db3
Steinardarri <Steinardth@gmail.com>
2019-10-16 22:15:03 +0000

    copy from projectB, database set

---
+++ src/main/java/is/hi/hbv501g/team21/Podypus/Services/Implementations/PodcastServiceImplementation.java
@@ -0,0 +1,52 @@
+package is.hi.hbv501g.team21.Podypus.Services.Implementations;
+
+import is.hi.hbv501g.team21.Podypus.Persistences.Entities.Podcast;
+import is.hi.hbv501g.team21.Podypus.Persistences.Repositories.PodcastRepository;
+import is.hi.hbv501g.team21.Podypus.Services.PodcastService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PodcastServiceImplementation implements PodcastService {
+
+    private PodcastRepository repository;
+
+    @Autowired
+    public PodcastServiceImplementation(PodcastRepository podcastRepository) {
+
+        this.repository = podcastRepository;
+    }
+
+    @Override
+    public Podcast save(Podcast podcast) {
+
+        return repository.save(podcast);
+    }
+
+    @Override
+    public void delete(Podcast podcast) {
+
+        repository.delete(podcast);
+    }
+
+    @Override
+    public List<Podcast> findAll() {
+
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<Podcast> findById(long id) {
+
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Podcast> findByTitle(String title) {
+
+        return repository.findByTitle(title);
+    }
+}
