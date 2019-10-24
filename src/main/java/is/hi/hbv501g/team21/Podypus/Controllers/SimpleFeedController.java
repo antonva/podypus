4c33a3787b3ceb5e2213e1d85b5039ea8e80481b
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2019-10-24 09:35:55 +0000

    Add basic query handling for itunes

--- src/main/java/is/hi/hbv501g/team21/Podypus/Controllers/SimpleFeedController.java
+++ src/main/java/is/hi/hbv501g/team21/Podypus/Controllers/SimpleFeedController.java
@@ -1,23 +1,45 @@
 package is.hi.hbv501g.team21.Podypus.Controllers;
 
+import is.hi.hbv501g.team21.Podypus.Persistences.Entities.SearchQuery;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @Controller
 public class SimpleFeedController {
 
-    @RequestMapping(value = "/feed", method = RequestMethod.GET)
+    //TODO: This needs to be part of a iTunes specific search service.
+    private final String ITUNES_SEARCH_URL = "https://itunes.apple.com/search?entity=podcast&term=";
+    //TODO: This needs to be part of a generic search service.
+    public static String urlEncode(String value) {
+        try {
+            return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException x) {
+            throw new RuntimeException(x.getCause());
+        }
+    }
+
+    @RequestMapping(value="/feed", method = RequestMethod.GET)
     public String feedHandler(Model model) {
+        model.addAttribute("query", new SearchQuery());
         return "Feed";
     }
 
     @RequestMapping(value = "/feed", method = RequestMethod.POST)
-    public String feedHandler(@Valid String url, BindingResult result, Model model) {
+    public String feedSubmit(@Valid SearchQuery query, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            System.out.println("Oh nooeees");
+        }
+        else {
+            model.addAttribute("query", query);
+            System.out.println(ITUNES_SEARCH_URL + urlEncode(query.getTerm()));
+        }
         return "Feed";
     }
 
