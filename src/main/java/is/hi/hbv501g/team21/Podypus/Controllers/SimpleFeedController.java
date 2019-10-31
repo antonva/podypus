85f5d1d67b39e0e806ef1b45438e3b88d93177fa
Steinar Darri Þorgilsson <steinardth@gmail.com>
2019-10-31 00:25:03 +0000

    got errors forward to sql

--- src/main/java/is/hi/hbv501g/team21/Podypus/Controllers/SimpleFeedController.java
+++ src/main/java/is/hi/hbv501g/team21/Podypus/Controllers/SimpleFeedController.java
@@ -7,13 +7,17 @@ import is.hi.hbv501g.team21.Podypus.Persistences.Entities.SearchResult;
 import is.hi.hbv501g.team21.Podypus.Services.PodcastService;
 import is.hi.hbv501g.team21.Podypus.Services.RssService;
 import is.hi.hbv501g.team21.Podypus.Services.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,15 +26,18 @@ public class SimpleFeedController {
     private SearchService searchService;
     private RssService rssService;
     private PodcastService podcastService;
+    private List<Podcast> p;
 
+    @Autowired
     public SimpleFeedController(SearchService searchService, RssService rssService,
                                 PodcastService podcastService) {
         this.searchService = searchService;
         this.rssService = rssService;
         this.podcastService = podcastService;
+        this.p = new ArrayList<>();
     }
 
-    @RequestMapping(value="/feed", method = RequestMethod.GET)
+    @RequestMapping(value = "/feed", method = RequestMethod.GET)
     public String feedHandler(Model model) {
         model.addAttribute("query", new SearchQuery());
         return "Feed";
@@ -40,31 +47,27 @@ public class SimpleFeedController {
     public String feedSubmit(@Valid SearchQuery query, BindingResult result, Model model) throws IOException {
         if (result.hasErrors()) {
             System.out.println("Oh nooeees");
-        }
-        else {
+        } else {
             model.addAttribute("query", query);
             SearchResult s = searchService.searchByTitle(query.getTerm());
             List<SearchItem> si = s.getResults();
-            Podcast p = rssService.parseFeed(si.get(0).getFeedUrl());
-            //model.addAttribute("results", s);
-            model.addAttribute("podcast", p);
-            System.out.println(p.toString());
+            p = rssService.parseManyFeeds(si);
+            model.addAttribute("results", s);
+            if (!p.isEmpty()) {
+                System.out.println(p.get(0).toString());
+            }
         }
         return "Feed";
     }
 
-    @RequestMapping(value = "/subscribe", method = RequestMethod.POST)
-    public String addPodcast(@Valid @ModelAttribute("podcast") Podcast podcast, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            //model.addAttribute("error");
-            System.out.println("Podcast subscribe error");
-            return "Feed";
-        }
-
+    @RequestMapping(value = "/subscribe/{nr}", method = RequestMethod.GET)
+    public String addPodcast(@PathVariable("nr") int nr, Model model) {
+        Podcast podcast = p.get(nr);
         model.addAttribute("podcast", podcast);
         podcastService.save(podcast);
+
         model.addAttribute("podcasts", podcastService.findAll());
-        return "Feed";
+        return "Home";
     }
 
 }
